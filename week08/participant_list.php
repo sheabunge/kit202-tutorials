730830518c9cf423946aebea966e57e71826410b
Shea Bunge <shea@sheabunge.com>
2021-04-21 01:34:35 +1000

    Move css and js to subdirectories

--- week08/participant_list.php
+++ week08/participant_list.php
@@ -95,7 +95,7 @@ include 'partials/registration.php';
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
-<script src="validate.js"></script>
+<script src="js/validate.js"></script>
 
 <script>
 	$(document).on('show.bs.modal', '#user-edit-modal', e => {
