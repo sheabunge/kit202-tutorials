93dd8600d4f7eb8764132ba62f6deaedcd68decb
Shea Bunge <shea@sheabunge.com>
2021-04-21 15:27:43 +1000

    Add helper script for augmenting database table

---
+++ week08/add_access.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This is a helper file, used only to augment the participant table and add the 'access' column.
+ *
+ * It is not part of the website, and the same task can alternatively be done manually through phpMyAdmin.
+ */
+
+require_once 'base.php';
+
+// add the new database table column.
+$mysqli->query( 'ALTER TABLE participant ADD (access ENUM("organiser", "participant") NOT NULL DEFAULT "participant")' );
+
+// set user 1 as an organiser.
+$mysqli->query( 'UPDATE participant SET access = "organiser" WHERE id = 1' );
