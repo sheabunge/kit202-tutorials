66ea0dc6d0e62c6d745a8c4f7159ed5cdd545d4c
Shea Bunge <shea@sheabunge.com>
2021-04-21 15:04:10 +1000

    Use include instead of include_once for template partials

--- week08/partials/footer.php
+++ week08/partials/footer.php
@@ -1,6 +1,6 @@
 <?php
-include_once 'registration.php';
-include_once 'login.php';
+include 'registration.php';
+include 'login.php';
 ?>
 
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
